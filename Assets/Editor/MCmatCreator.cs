7b7b99d2ecbdda92fccab8e63903e19a5fafda9f
NgbatzYT <94680235+NgbatzYT@users.noreply.github.com>
2025-09-23 23:20:35 +1000

    Add files via upload

--- Assets/Editor/MCmatCreator.cs
+++ Assets/Editor/MCmatCreator.cs
@@ -9,10 +9,10 @@ public class MCmatCreator
     {
         string dir = "Assets/McMats";
 
-        if (!Directory.Exists(dir))
-        {
-            Directory.CreateDirectory(dir);
-        }
+        Directory.Delete(dir, true);
+        Directory.CreateDirectory(dir);
+        
+        
         BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 
         ChangeExtension(dir, ".MCmat");
@@ -22,16 +22,11 @@ public class MCmatCreator
     {
         foreach (string file in Directory.GetFiles(directory))
         {
-            if (Path.GetExtension(file) == ".manifest" || Path.GetExtension(file) == ".meta") continue;
+            if (Path.GetExtension(file) == ".manifest" || Path.GetExtension(file) == ".meta" || Path.GetFileNameWithoutExtension(file) == "McMats") continue;
 
             string newFile = Path.ChangeExtension(file, ext);
 
-            if (File.Exists(newFile))
-            {
-                File.Delete(newFile);
-            }
-
-            File.Move(file, newFile);
+            File.Copy(file, newFile, true); 
         }
     }
 }
